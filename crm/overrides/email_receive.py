d581d7402034644dfa33d382944810ce24723abe
Nidheesh P <nidheeshdevops@gmail.com>
2025-09-20 07:08:18 +0530

    feat: enhance email message retrieval to include folder context and improve UID validity checks

--- crm/overrides/email_receive.py
+++ crm/overrides/email_receive.py
@@ -24,7 +24,7 @@ def custom_get_messages(self, folder="INBOX"):
 
 		for i, uid in enumerate(email_list[:num]):
 				try:
-						self.retrieve_message(uid, i + 1)
+						self.retrieve_message(uid, i + 1, folder)
 				except (_socket.timeout, LoginLimitExceeded):
 						break
 
@@ -36,4 +36,35 @@ def custom_get_messages(self, folder="INBOX"):
 
 		return out
 
+def custom_check_imap_uidvalidity(self, folder):
+		# compare the UIDVALIDITY of email account and imap server
+		uid_validity = cint(self.settings.uid_validity)
+
+		response, message = self.imap.status(folder, "(UIDVALIDITY UIDNEXT)")
+		current_uid_validity = cint(self.parse_imap_response("UIDVALIDITY", message[0]))
+
+		uidnext = int(self.parse_imap_response("UIDNEXT", message[0]) or "1")
+
+		if uid_validity is None:
+			frappe.flags.initial_sync = True
+
+		if not uid_validity or uid_validity != current_uid_validity:
+			# uidvalidity changed & all email uids are reindexed by server
+			Communication = frappe.qb.DocType("Communication")
+			frappe.qb.update(Communication).set(Communication.uid, -1).where(
+				Communication.communication_medium == "Email"
+			).where(Communication.email_account == self.settings.email_account).run()
+
+			# new update for the IMAP Folder DocType
+			IMAPFolder = frappe.qb.DocType("IMAP Folder")
+			frappe.qb.update(IMAPFolder).set(IMAPFolder.uidvalidity, current_uid_validity).set(
+				IMAPFolder.uidnext, uidnext
+			).where(IMAPFolder.parent == self.settings.email_account_name).where(
+				IMAPFolder.folder_name == folder
+			).run()
+
+			self.settings.email_sync_rule = "ALL"
+			self.uid_reindexed = True
+
 EmailServer.get_messages = custom_get_messages
+EmailServer.check_imap_uidvalidity = custom_check_imap_uidvalidity
